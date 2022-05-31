e68da429ec090b9e72949200934391c1fda9be02
Marcelo de Jesus Nunez <chelo154@gmail.com>
2022-05-31 16:42:45 -0300

    Fixed OS Selection

--- CodeEvaluatorKernel/src/utils/os_utils.py
+++ CodeEvaluatorKernel/src/utils/os_utils.py
@@ -1,12 +1,13 @@
+import platform
 import os
 
 
 def get_tempdir() -> str:
-    return os.environ['TEMP']
+    return os.environ['TEMP'] if 'Windows' in platform.system() else '/tmp'
 
 
 def get_file_separator() -> str:
-    return '\\' if 'WINDOWS' in os.environ['OS'] else '/'
+    return '\\' if 'Windows' in platform.system() else '/'
 
 
 def get_os_name() -> str:
