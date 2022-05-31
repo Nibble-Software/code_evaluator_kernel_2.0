1f0a744f3d5d767bbcd9b6935112f357bcc7306f
Marcelo de Jesus Nunez <chelo154@gmail.com>
2022-05-30 23:19:22 -0300

    Fixed py_runner.py problem

--- CodeEvaluatorKernel/src/runners/py_runner.py
+++ CodeEvaluatorKernel/src/runners/py_runner.py
@@ -1,5 +1,5 @@
 from src.utils.process_utils import run_code
 
 
-def run_py(filepath: str, inputs: list[str]) -> [str]:
+def run_py(filepath: str, inputs: [str]) -> [str]:
     return run_code('python', [filepath], inputs)
